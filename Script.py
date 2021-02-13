c8cbdd6e90a968f5315df67e08e5121f6220b4a8
ctrl-gaurav <gauravhhh30@gmail.com>
2021-02-13 13:18:51 +0530

    added recommender script and dataset

---
+++ Script.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import sigmoid_kernel
+
+
+credits = pd.read_csv("tmdb_5000_credits.csv")
+movies_df = pd.read_csv("tmdb_5000_movies.csv")
+
+credits_column_renamed = credits.rename(index=str, columns={"movie_id": "id"})
+movies_df_merge = movies_df.merge(credits_column_renamed, on='id')
+movies_cleaned_df = movies_df_merge.drop(columns=['homepage', 'title_x', 'title_y', 'status','production_countries'])
+
+
+# Using arguments for TF-IDF
+tfv = TfidfVectorizer(min_df=3,  max_features=None, strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',ngram_range=(1, 3),stop_words = 'english')
+
+
+# Filling NaNs with empty string
+movies_cleaned_df['overview'] = movies_cleaned_df['overview'].fillna('')
+
+
+# Fitting the TF-IDF on the 'overview' text
+tfv_matrix = tfv.fit_transform(movies_cleaned_df['overview'])
+
+
+# Compute the sigmoid kernel
+sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
+
+
+# Reverse mapping of indices and movie titles
+indices = pd.Series(movies_cleaned_df.index, index=movies_cleaned_df['original_title']).drop_duplicates()
+
+
+def generate_rec_movies(title, sig=sig):
+    # Get the index corresponding to original_title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores 
+    sig_scores = list(enumerate(sig[idx]))
+
+    # Sort the movies 
+    sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
+
+    # Scores of the 10 most similar movies
+    sig_scores = sig_scores[1:11]
+
+    # Movie indices
+    movie_indices = [i[0] for i in sig_scores]
+
+    # Top 10 most similar movies
+    return movies_cleaned_df['original_title'].iloc[movie_indices]
+
+
+# Get array of recommended movies 
+def recommendations(str):
+    movies=[np.array(generate_rec_movies(str))]
+    return movies
+
+
+str = input("enter movie name : ")
+print(recommendations(str))
